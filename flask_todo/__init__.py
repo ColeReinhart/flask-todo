a1e6f25a38ccb14ed38f85a40d30df035e145bb9
Cole Reinhart <colecooler16@hotmail.com>
2019-03-28 16:20:15 -0400

    -Added a tasks page -made adding smoother

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,51 +1,43 @@
 from flask import Flask, render_template, request
 import time, psycopg2
 
-# connection to database
-con = psycopg2.connect(
-            host = "localhost",
-            database="flask_todo"
-)
-#cursor
-cur = con.cursor()
-
-# execute query
-cur.execute("select * from todos")
-
-rows = cur.fetchall()
-
-for r in rows:
-    print(r)
-#commiting changes
-
+localtime = time.asctime( time.localtime(time.time()) )
 
 def create_app():
     app = Flask(__name__)
+    # connection to database
+    con = psycopg2.connect(
+                host = "localhost",
+                database="flask_todo"
+    )
+    #cursor
+    cur = con.cursor()
 
-    @app.route('/')
-    def index():
-        return f'welcome'
+    # execute query
+    
 
-
-    @app.route('/todo', methods=['GET', 'POST'])
+    @app.route('/', methods=['GET', 'POST'])
     def todo():
-        result = ""
         if request.method == 'POST':
             result = request.form['task']
-        cur.execute("INSERT INTO todos (task, completed, created) VALUES ('{result}', 'no', 'today')")
-        con.commit()
-
-        return render_template('todo.html', result=rows)
-
+            cur.execute("INSERT INTO todos (task, completed, created) VALUES (%s,%s,%s)",(result, False, localtime))
+            con.commit()
+  
+        return render_template('todo.html')
+    @app.route('/tasks', methods=['GET','POST'])
+    def task():
+        cur.execute("select task,created from todos")
+        rows = cur.fetchall()
+       
+        return render_template('tasks.html', thing=rows)
     @app.route('/completed', methods=['GET'])
     def completed():
-        # todos = {'thing': 'clean garage', 'completed': 'False'}
-        # variables = [todos['thing'], todos['completed']]
+        cur.execute("select task,created from todos WHERE completed = 'yes'")
+        rows = cur.fetchall()
+
         return render_template('completed.html', thing=rows)
 
-    
+  
     return app
-    #close the cursor
-    cur.close()
-    # close the connection
     con.close()
+    cur.close()
